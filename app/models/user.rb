64f6ddb1a3e70c2ecb1f00352fa4aca16d0107e9
bkapusta <kapusta.borna.s@gmail.com>
2018-03-04 19:40:41 +0100

    Destroy attendances with user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one :membership_type
-  has_many :member_attendances
+  has_many :member_attendances, :dependent => :delete_all
   has_many :group_has_users
   has_many :groups, :through => :group_has_users
 
